2d2d3d4ece7de193ab4d989bfb934d428c4fe224
Robert Klep <robert@klep.name>
2021-04-11 14:43:40 +0200

    Added client for Bosch EasyControl (CT200)

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 require('tls').convertNPNProtocols = null;
 
 module.exports = {
-  BaseClient:      require('./lib/base-client'),
-  IVTClient:       require('./lib/ivt-client'),
-  NefitEasyClient: require('./lib/nefit-easy-client'),
+  BaseClient:        require('./lib/base-client'),
+  IVTClient:         require('./lib/ivt-client'),
+  NefitEasyClient:   require('./lib/nefit-easy-client'),
+  EasyControlClient: require('./lib/easycontrol-client'),
 };
